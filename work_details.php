c2127a5e3b7ec80bdd473f557edce458fdd096f8
hakem26 <hamed26km@gmail.com>
2025-03-10 15:10:38 +0330

    006-21

--- work_details.php
+++ work_details.php
@@ -338,12 +338,7 @@ if ($selected_year) {
             <div class="alert alert-warning text-center">اطلاعاتی وجود ندارد.</div>
         <?php endif; ?>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="assets/js/persian-date.min.js"></script>
-    <script src="assets/js/persian-datepicker.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
     <script>
         $(document).ready(function() {
             $('.agency-select').change(function() {
