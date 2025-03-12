f01eba0e75c807de155e6feb974b6e3a6db0a981
hakem26 <hamed26km@gmail.com>
2025-03-12 14:01:27 +0330

    8-17

--- product_summary.php
+++ product_summary.php
@@ -329,9 +329,6 @@ foreach ($products as $product) {
     <?php endif; ?>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#productsTable').DataTable({
