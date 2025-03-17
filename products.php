b52c4efefd7b786d30d6a6ef97811fc6c61f06d5
hakem26 <hamed26km@gmail.com>
2025-03-17 09:15:13 +0330

    13-6

--- products.php
+++ products.php
@@ -337,7 +337,7 @@ table.dataTable tbody td {
                 }
             },
             "columnDefs": [
-                { "targets": "_all", "className": "text-right" }, // وسط‌چین کردن همه ستون‌ها
+                { "targets": "_all", "className": "text-start" }, // وسط‌چین کردن همه ستون‌ها
                 { "targets": 0, "width": "50px" },  // شناسه
                 { "targets": 1, "width": "200px" }, // نام محصول
                 { "targets": 2, "width": "120px" }  // قیمت واحد
