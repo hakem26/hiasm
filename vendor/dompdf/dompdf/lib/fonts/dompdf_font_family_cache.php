9b41221228b5233656dbcc611207c399ef63d9db
hakem26 <hamed26km@gmail.com>
2025-03-17 16:34:49 +0330

    13-36

--- vendor/dompdf/dompdf/lib/fonts/dompdf_font_family_cache.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'vazirmatn' => [
-        'normal' => realpath(__DIR__ . './Vazirmatn-Regular.ttf'),
-        'bold' => realpath(__DIR__ . './Vazirmatn-Regular.ttf'),
-        'italic' => realpath(__DIR__ . './Vazirmatn-Regular.ttf'),
-        'bold_italic' => realpath(__DIR__ . './Vazirmatn-Regular.ttf'),
-    ],
-];
