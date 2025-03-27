c0d37aa369155bd56b32f79e8aeaf60048143ca1
hakem26 <hamed26km@gmail.com>
2025-03-28 00:42:13 +0330

    22-8

--- db.php
+++ db.php
@@ -1,18 +1,14 @@
 <?php
 // [BLOCK-DB-001]
 $host = 'localhost';
-// main
 $dbname = 'yshcvdau_hiasm';
 $username = 'yshcvdau_hiasmadmin';
-// test
-// $dbname = 'ukvojota_hiasm';
-// $username = 'ukvojota_hiasmadmin'; 
 $password = 'H72j51300!';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("اتصال به دیتابیس失败: " . $e->getMessage());
+    die("اتصال به دیتابیس شکست: " . $e->getMessage());
 }
 ?>
